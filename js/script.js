dd30be0d374844b8df2be1460c5eab718116d910
Martina Cappelloni <martinacappelloni@Martinas-MBP.fibertel.com.ar>
2020-05-27 18:14:52 -0300

    detail tracks

--- js/script.js
+++ js/script.js
@@ -16,10 +16,10 @@ fetch(apiRoute + '/chart')
         contenido += '<li class="track">'
         contenido += '<img class="track-img" src="' + element.album.cover + '"alt="album-cover">'
         contenido += '<h2 class="title">'
-        contenido += '<a href="detail-tracks.html">' + element.title + '</a>'
+        contenido += '<a href="detail-tracks.html?id='+ element.id + '">' + element.title + '</a>'
         contenido += '</h2>'
         contenido += '<h3 class="artist">'
-        contenido += '<a href="detail-artists.html">' + element.artist.name + '</a>'
+        contenido += '<a href="detail-artists.html?id=' + element.artist.id + '">' + element.artist.name + '</a>'
         contenido += '</h3>'
         contenido += '</li>'
     }
---
+++ js/track-detail.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+    
+    const apiRoute = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/';
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    fetch(apiRoute + '/track/' + id )
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+
+        console.log(data)
+        var contenedor = document.querySelector('.contenido')
+        var contenido = ' '
+
+        contenido += '<div class="track-img">'
+        contenido +=  '<img src="'+ data.album.cover +'" alt="track-img"></img>'
+        contenido += '</div>'
+        contenido += '<div style="width: 100%;">'
+        contenido += '<div class="info">'
+        contenido += '<ul class="list">'
+        contenido += '<li class="title">' + data.title + '</li>'
+        contenido += '<li class="subtitle">'
+        contenido += '<a href="detail-albums.html">' + data.album.title + '</a>'
+        contenido += '</li>'
+        contenido += '<li class="date">' + data.release_date + '</li>'
+        contenido += '<section class="add">'
+        contenido += '<li class="heart">'
+        contenido += '<a href=""><i class="far fa-heart"></i></a>'
+        contenido += '</li>'
+        contenido += '<li class="plus">'
+        contenido += '<a href="playlist.html"><i class="fas fa-plus"></i></a>'
+        contenido += '</li>'
+        contenido += '<li class="elipsis">'
+        contenido += '<a href=""><i class="fas fa-ellipsis-h"></i></a>'
+        contenido += '</li>'
+        contenido += '</section>'
+        contenido += '</ul>'
+        contenido += '</div>'
+        contenido += '<div class="canciones">'
+        contenido += '<ul class="cancion">'
+        contenido += '<li class="song">' + data.title + '</li>'
+        contenido += '<li class="artist">' + data.artist.name + '</li>'
+        contenido += '</ul>'
+        contenido += '</div>'
+        contenido += '</div>'
+
+        contenedor.innerHTML = contenido;
+
+    })
+    .catch(function (error) {
+        console.log("Hubo un error" + error);
+    })
+
+
+
+}
