aa17a619929db4212a919c7c73771accebe62e17
francisco dimo <fdimo@udesa.edu.ar>
2020-05-26 21:27:00 -0300

    fetch albums

--- js/script.js
+++ js/script.js
@@ -30,17 +30,36 @@ fetch(apiRoute + '/chart')
 })
 
 
+fetch(apiRoute + '/chart')
+.then(function (response) {
+    return response.json();
+})
+.then(function (informacion) {
+    console.log(informacion.albums.data);
+    var contenedor = document.querySelector('.lista-albums');
+    console.log(contenedor);
+    var contenido = " ";
+    for (let i = 0; i < informacion.albums.data.length; i++) {
+        var element = informacion.albums.data[i];
+        contenido += '<li class="album">'
+        contenido += '<img class="album-img" src="' + element.cover + '"alt="album-cover">'
+        contenido += '<h2 class="title">'
+        contenido += '<a href="detail-albums.html">' + element.title + '</a>'
+        contenido += '</h2>'
+        contenido += '<h3 class="artist">'
+        contenido += '<a href="detail-artists.html">' + element.artist.name + '</a>'
+        contenido += '</h3>'
+        contenido += '</li>'
+    }
+    contenedor.innerHTML = contenido;
+})
+.catch(function (error) {
+    console.log("Hubo un error" + error);
+})
 
-                // <li class="album">
-                //     <img class="album-img" src="img/lewis.png" alt="divinely-uninspired-to-a-hellish-extent">
-                //     <h2 class="title">
-                //         <a href="detail-albums.html">divinely uninspired to a hellish extent</a>
-                //     </h2>
-                //     <h3 class="artist">
-                //         <a href="detail-artists.html">lewis capaldi</a>
-                //     </h3>
-                // </li>
 
+
+     
 
    
    
