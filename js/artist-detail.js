02a5accc03615f0512aa0ab80bccdfe0618ca40d
Mora Baylac <morabaylac@MacBook-Pro-de-Mora.local>
2020-05-27 19:53:05 -0300

    js

--- js/artist-detail.js
+++ js/artist-detail.js
@@ -17,12 +17,12 @@ window.onload = function () {
     
     contenido += '<aside style="width: 100%;">'
     contenido += '<div class="artista-img">'
-    contenido += '<img src="img/beatles.jpg" alt="artista-img">'
+    contenido += '<img src="'+ data.picture +'" alt="artista-img">'
     contenido += '</div>'
     contenido += '<div class="info">'
     contenido += '<ul class="list">'
-    contenido += '<li class="title">the beatles</li>'
-    contenido += '<li class="followers">22,977,599</li>'
+    contenido += '<li class="title"> ' + data.name + '</li>'
+    contenido += '<li class="followers">' + data.nb_fan + '</li>'
     contenido +='<section class="add">'
     contenido += '<li class="heart">'
     contenido += '<a href=""><i class="far fa-heart"></i></a>'
@@ -38,20 +38,18 @@ window.onload = function () {
     contenido += '</div>'
     contenido += '</aside>'
 
-        for (let i = 0; i < data.length; i++) {
-    var element = data [i];
-    contenido += '<div class="canciones">'
-    contenido += '<ul class="cancion">'
-    contenido += '<a href="detail-tracks.html">'
-    contenido += '<li class="song">here comes the sun</li>'
-    contenido += '<li class="artist">remastered 2009</li>'
-    contenido += '</a>'
-    contenido += '</ul>'
-    contenido += '</div>'
+//         for (let i = 0; i < data.length; i++) {
+//     var element = data [i];
+//     contenido += '<div class="canciones">'
+//     contenido += '<ul class="cancion">'
+//     contenido += '<a href="detail-tracks.html">'
+//     contenido += '<li class="song">' + element.tracklist + '</li>'
+//     contenido += '<li class="artist">remastered 2009</li>'
+//     contenido += '</a>'
+//     contenido += '</ul>'
+//     contenido += '</div>'
 
-
-
-}
+// }
 contenedor.innerHTML = contenido;
 })
 
